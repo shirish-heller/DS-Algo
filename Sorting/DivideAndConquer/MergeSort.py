e72cedb47e71eb5ec96d38e722b5311039aa383a
Rajolia <Shivansh_Rajolia@intuit.com>
2020-09-19 11:11:41 +0530

    merge sort

---
+++ Sorting/DivideAndConquer/MergeSort.py
@@ -0,0 +1,33 @@
+def mergeSort(a):
+    if(len(a)==1):
+        return a
+    elif(len(a)==2):
+        if(a[0]<a[1]):
+            return a
+        else: return [a[1], a[0]]
+    else:
+        splitPoint=len(a)//2
+        topSortedArr=mergeSort(a[0:splitPoint])
+        bottomSortedArr=mergeSort(a[(splitPoint):len(a)])
+        i,j=0,0
+        mergedArr=[]
+
+        # Merging Sorted Arrays
+        while(1):
+            if(i>=len(topSortedArr)):
+                mergedArr.extend(bottomSortedArr[j:len(bottomSortedArr)])
+                break
+            if(j>=len(bottomSortedArr)):
+                mergedArr.extend(topSortedArr[i:len(topSortedArr)])
+                break
+            if(topSortedArr[i]<bottomSortedArr[j]):
+                mergedArr.append(topSortedArr[i])
+                i=i+1
+            else:
+                mergedArr.append(bottomSortedArr[j])
+                j=j+1
+        return mergedArr
+                    
+# TESTING
+inpArr=[5,4,6,2,9,3,1]
+print(mergeSort(inpArr))
